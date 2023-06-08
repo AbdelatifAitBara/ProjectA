9a73e961f8fc0ed3ead808d571cc64e9b9a3a67e
AbdelatifAitBara <aitbaraabdealtif@outlook.com>
2023-06-08 19:43:01 +0200

    Edited pem_generate.sh, added pictures folder

--- pem_generate.sh
+++ pem_generate.sh
@@ -11,12 +11,7 @@ DOMAIN=$1
 
 # Create root CA & Private key
 
-openssl req -x509 \
-            -sha256 -days 356 \
-            -nodes \
-            -newkey rsa:2048 \
-            -subj "/CN=${DOMAIN}/C=US/L=San Fransisco" \
-            -keyout rootCA.key -out rootCA.crt
+openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=Odoo16/C=FR/L=Lyon" -keyout rootCA.key -out rootCA.crt
 
 # Generate Private key
 
@@ -71,14 +66,11 @@ EOF
 
 # Create SSl with self signed CA
 
-openssl x509 -req \
-    -in ${DOMAIN}.csr \
-    -CA rootCA.crt -CAkey rootCA.key \
-    -CAcreateserial -out ${DOMAIN}.crt \
-    -days 365 \
-    -sha256 -extfile cert.conf
+openssl x509 -req -in ${DOMAIN}.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out ${DOMAIN}.crt -days 365 -sha256 -extfile cert.conf
 
 
 # Generate the pem file
 
 cat mydomain.key mydomain.crt rootCA.key > mydomain.pem
+
+rm -f cert.conf mydomain.crt mydomain.key rootCA.crt rootCA.srl csr.conf mydomain.csr rootCA.key
